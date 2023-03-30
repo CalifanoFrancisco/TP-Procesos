da5cca437441c76bc8c242fda7a1846dc3f8c572
Kaligrámetro <81710127+CalifanoFrancisco@users.noreply.github.com>
2023-03-29 21:47:36 -0300

    Update create_schema.sql

--- DiseñoBaseDeDatos/DisenoBaseDeDatos/create_schema.sql
+++ DiseñoBaseDeDatos/DisenoBaseDeDatos/create_schema.sql
@@ -4,6 +4,7 @@ CREATE DATABASE sistemaPiezas;
 CREATE TABLE `sistemaPiezas`.`Piezas` (
     `id`            INT NOT NULL AUTO_INCREMENT,
     `material`      VARCHAR(50),
+    `peso`          INT NOT NULL DEFAULT 0
     PRIMARY KEY (`id`)
 );
 
