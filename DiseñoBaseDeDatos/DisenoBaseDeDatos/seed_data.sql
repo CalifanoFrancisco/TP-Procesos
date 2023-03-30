3de1cacdc2c597c3e93eb36e368ce614739ed3ae
Kaligrámetro <81710127+CalifanoFrancisco@users.noreply.github.com>
2023-03-30 14:41:45 -0300

    Update seed_data.sql

--- DiseñoBaseDeDatos/DisenoBaseDeDatos/seed_data.sql
+++ DiseñoBaseDeDatos/DisenoBaseDeDatos/seed_data.sql
@@ -1,12 +1,12 @@
-INSERT INTO `sistemaPiezas`.`Piezas` 
+INSERT INTO `SistemaDePiezas`.`Piezas` 
     (`material`)
 VALUES
-    ('rueda'),
-    ('acero'),
-    ('goma')
+    ('rueda', 10),
+    ('acero', 20),
+    ('goma', 30)
 ;
 
-INSERT INTO `sistemaPiezas`.`PiezasLinker`
+INSERT INTO `SistemaDePiezas`.`PiezasLinker`
     (`id_pieza`, `id_componente`)
 VALUES
 -- rueda(1) esta hecha de acero(2) y goma(3)
@@ -14,7 +14,7 @@ VALUES
     (1, 3)
 ;
 
-INSERT INTO `sistemaPiezas`.`Procesos` 
+INSERT INTO `SistemaDePiezas`.`Procesos` 
     ( `id_pieza_salida`, `tipo`)
 VALUES
     (1, 'ensamble')
