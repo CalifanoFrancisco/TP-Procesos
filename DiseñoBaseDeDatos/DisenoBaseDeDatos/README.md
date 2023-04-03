67af2f29404fac62d93d26231189e1a8ba943990
Kaligrámetro <81710127+CalifanoFrancisco@users.noreply.github.com>
2023-04-03 14:48:02 -0300

    Delete DiseñoBaseDeDatos directory

--- DiseñoBaseDeDatos/DisenoBaseDeDatos/README.md
+++
@@ -1,4 +0,0 @@
-Para inicializar la base de datos ejecutar los scripts en este orden:
-1. @create_schema.sql
-2. @create_user.sql
-3. @seed_data.sql
--- DiseñoBaseDeDatos/DisenoBaseDeDatos/create_user.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'PiezasManager'@'localhost' IDENTIFIED BY 'shiro_gaming';
-
-GRANT ALL PRIVILEGES ON teams_system.teams TO 'PiezasManager'@'localhost';
-
-flush privileges;
