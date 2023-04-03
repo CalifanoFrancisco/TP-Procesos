9ee69be604f4f550d039349533c09fb7a9c6d71e
Kaligrámetro <81710127+CalifanoFrancisco@users.noreply.github.com>
2023-04-03 17:35:15 -0300

    Update index.ts

--- Funciona/index.ts
+++ Funciona/index.ts
@@ -1,9 +1,35 @@
 import * as dotenv            from "dotenv";
 import      express           from "express";
 import * as bodyParser        from "body-parser";
+
 import { PiezaRouter }        from "./routes/PiezaRouter";
 import { PiezasLinkerRouter } from "./routes/PiezasLinkerRouter";
 import { ProcesoRouter }      from "./routes/ProcesoRouter"
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+const swaggerOptions = {
+    swaggerDefinition:{
+        info:{
+            version: "1.0.0",
+            title: "Docu API",
+            description: "API Documentation for use",
+            contact:{
+                name: "Peppino Spagetti",
+                url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+            },
+            servers: ["http://localhost:3000"]
+        }
+    },
+    basePath: "/",
+    apis:[
+        "./routes/PiezaRouter.ts",
+        "./routes/PiezasLinkerRouter.ts",
+        "./routes/ProcesoRouter.ts",
+    ]
+};
+
 
 const app = express();
 dotenv.config();
@@ -13,6 +39,10 @@ app.use("/piezas",       PiezaRouter);
 app.use("/piezaslinker", PiezasLinkerRouter);
 app.use("/procesos",     ProcesoRouter);
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.listen(process.env.PORT, () => {
     console.log(`Node server running in port ${process.env.PORT}! (๑✪ . ✪๑)`);
 });
+
+module.exports = app;
