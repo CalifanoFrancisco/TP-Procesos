d46a5b7c50495ad9f011249560062ec067782560
Kaligrámetro <81710127+CalifanoFrancisco@users.noreply.github.com>
2023-04-17 17:18:32 -0300

    Add files via upload

---
+++ Funciona2/types/PiezaLink.ts
@@ -0,0 +1,8 @@
+export class PiezaLink  {
+    id_pieza:Number;
+    id_componente:Number;
+    constructor(_id_pieza:Number, _id_componente:Number) {
+        this.id_pieza      = _id_componente;
+        this.id_componente = _id_componente;
+    }
+}
