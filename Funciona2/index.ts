39c1feb07aa571283092047e3f4fe40ebb041f71
Kaligrámetro <81710127+CalifanoFrancisco@users.noreply.github.com>
2023-04-18 17:16:19 -0300

    Update index.ts

--- Funciona2/index.ts
+++ Funciona2/index.ts
@@ -4,7 +4,10 @@ import * as bodyParser from "body-parser";
 
 import { PiezasLinkerRouter } from "./routes/PiezasLinkerRouter";
 import { ProcesoRouter }      from "./routes/ProcesoRouter";
-import { PiezaRouter }        from "./routes/PiezaRouter"
+import { PiezaRouter }        from "./routes/PiezaRouter";
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 dotenv.config();
@@ -13,7 +16,9 @@ app.use(bodyParser.json());
 
 app.use("/piezas", PiezaRouter);
 app.use("/procesos", ProcesoRouter);
-app.use("/piezasLinker", PiezasLinkerRouter)
+app.use("/piezasLinker", PiezasLinkerRouter);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(process.env.PORT, () => {
     console.log(`Node server running in port ${process.env.PORT}! (๑✪ . ✪๑)`);
