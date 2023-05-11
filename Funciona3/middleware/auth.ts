f2ed475a34938f79c434b34f762e1dae90a27d47
Kaligrámetro <81710127+CalifanoFrancisco@users.noreply.github.com>
2023-05-11 14:42:11 -0300

    Update auth.ts

--- Funciona3/middleware/auth.ts
+++ Funciona3/middleware/auth.ts
@@ -1,24 +1,17 @@
-import jwt,     { Secret, JwtPayload } from 'jsonwebtoken'
-import express, { Request, Response, NextFunction  } from 'express'
-import { Usuario } from '../types/Usuario';
+import { Request, Response, NextFunction  } from 'express'
+import jwt from "jsonwebtoken"
 
-export const SECRET_KEY:Secret = "llave-secreta-jeje-uwu";
-
-export interface CustomRequest extends Request {
-    token: string | JwtPayload;
-}
+export const SECRET_KEY = "llave-secreta-jeje-uwu";
 
 export async function auth(req:Request, res:Response, next:NextFunction) {
+
+    const token = req.headers['authorization'];
+
     try {
-        const token = req.header('Autorization')?.replace('Bearer ', '');
-        //const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoic29uUm9ib3RzIiwicGFzc3dvcmQiOiJzb25Sb2JvdHMiLCJzYWx0IjowLCJpYXQiOjE2ODM1NzIxMzEsImV4cCI6MTY4Mzc0NDkzMX0.x9R4YtFjxyXnrDhzc8oDbR-18pD1UIy3ws_7aebRqEk"
-
         if (!token) throw new Error();
-
-        const decoded = jwt.verify(token, SECRET_KEY);
-        (req as CustomRequest).token = decoded;
-
-        console.log(decoded);        
+        
+        const decoded = jwt.verify(token, SECRET_KEY);  
+        
         next();
 
     } catch (err) {
